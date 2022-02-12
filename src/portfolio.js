8259c30f0ee77b09d524f330fab692e01cf802a6
MSLA-Manav <Manav.Mandal@studentambassadors.com>
2022-02-12 17:13:15 +0530

    Removed publications section from projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -376,12 +376,7 @@ const projectsHeader = {
   avatar_image_path: "projects_image.svg",
 };
 
-const publicationsHeader = {
-  title: "Publications",
-  description:
-    "I have worked on and published a few research papers and publications of my own.",
-  avatar_image_path: "projects_image.svg",
-};
+
 
 // Contact Page
 const contactPageData = {
@@ -389,25 +384,25 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "animated_ashutosh.png",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "I am here to learn and grow so lets connect. I can help you with ML, AI, and Azure Cloud.",
   },
   blogSection: {
     title: "Blogs",
     subtitle:
       "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
-    link: "https://ashutoshhathidara.wordpress.com",
+      link: "https://manavmandal.medium.com/",
     avatar_image_path: "blogs_image.svg",
   },
   addressSection: {
     title: "Address",
     subtitle:
-      "Ambavadi vas, Kanodar, T.A.-Palanpur, Dist.-Banaskantha, Gujarat - 385520",
+      "Vadgaonsheri, Pune, Maharashtra 411014",
     avatar_image_path: "address_image.svg",
-    location_map_link: "https://goo.gl/maps/MpMqtRZytFchMkZ76",
+      location_map_link: "https://goo.gl/maps/zrEwAu3FzugKv6U57",
   },
   phoneSection: {
     title: "Phone Number",
-    subtitle: "+91 8320758513",
+    subtitle: "+91 9588412823",
   },
 };
 
@@ -421,7 +416,6 @@ export {
   degrees,
   certifications,
   experience,
-  projectsHeader,
-  publicationsHeader,
+  projectsHeader,  
   contactPageData,
 };
